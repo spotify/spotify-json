71a3c6f3408f018ee31504206ba1ea7101daf3f7
Johan Lindström <johanl@spotify.com>
2014-04-02 01:53:10 +0200

    Tweak the writer a bit to make it a few % faster.
    Most of the performance improvement comes from less churn on
    the _separator_needed variable. No reason to continuously unset
    and set it. Some smaller tweaks also, like only checking the value
    once in the <<bool stream operation and storing the end iterators.

--- json_writer.hpp
+++ json_writer.hpp
@@ -27,63 +27,66 @@ template<typename stream_type>
 class writer {
  public:
   explicit writer(stream_type &stream)
-    : _separator_needed(false),
-      _stream(stream),
+    : _stream(stream),
+      _separator_needed(false),
       _scoped_locale(LC_NUMERIC_MASK, "C") {}
 
   virtual ~writer() {}
 
   template<typename T>
   writer &operator <<(const T &value) {
-    return separator().write(value).set_separator(true);
+    return separator_and_set().write(value);
   }
 
   writer &operator <<(const null_type &) {
-    return separator().write("null", 4).set_separator(true);
+    return separator_and_set().write("null", 4);
   }
 
   writer &operator <<(bool value) {
-    return separator().write(value ? "true" : "false", value ? 4 : 5).set_separator(true);
+    return (value ?
+        separator_and_set().write("true", 4) :
+        separator_and_set().write("false", 5));
   }
 
   writer &operator <<(int8_t value) {
-    return separator().write(static_cast<signed>(value)).set_separator(true);
+    return separator_and_set().write(static_cast<signed>(value));
   }
 
   writer &operator <<(uint8_t value) {
-    return separator().write(static_cast<unsigned>(value)).set_separator(true);
+    return separator_and_set().write(static_cast<unsigned>(value));
   }
 
   writer &operator <<(const key &key) {
-    return separator().write(key.data, key.size).set_separator(true);
+    return separator_and_set().write(key.data, key.size);
   }
 
   writer &operator <<(const char *value) {
-    separator().put('"');
+    separator_and_set().put('"');
     write_escaped(_stream, value, null_terminated_end_iterator());
-    return put('"').set_separator(true);
+    return put('"');
   }
 
   writer &operator <<(const std::string &value) {
-    separator().put('"');
+    separator_and_set().put('"');
     write_escaped(_stream, value.begin(), value.end());
-    return put('"').set_separator(true);
+    return put('"');
   }
 
   template<typename K, typename V>
   writer &operator <<(const pair<K, V> &pair) {
-    return (*this << pair.key).set_separator(false).put(':') << pair.value;
+    return (*this << pair.key).clear_separator().put(':') << pair.value;
   }
 
   template<typename T, typename U>
   writer &operator <<(const std::pair<T, U> &pair) {
-    return (*this << pair.first).set_separator(false).put(':') << pair.second;
+    return (*this << pair.first).clear_separator().put(':') << pair.second;
   }
 
   template<typename T>
   writer &operator <<(const std::vector<T> &vector) {
-    scoped_array arr(*this);
-    for (typename std::vector<T>::const_iterator it = vector.begin(); it != vector.end(); ++it) {
+    const scoped_array array(*this);
+    typedef typename std::vector<T>::const_iterator const_iterator;
+    for (const_iterator it = vector.begin(), end = vector.end(); it != end; ++it) {
       *this << *it;
     }
     return *this;
@@ -91,8 +94,9 @@ class writer {
 
   template<typename T>
   writer &operator <<(const std::set<T> &set) {
-    scoped_array arr(*this);
-    for (typename std::vector<T>::const_iterator it = set.begin(); it != set.end(); ++it) {
+    const scoped_array array(*this);
+    typedef typename std::vector<T>::const_iterator const_iterator;
+    for (const_iterator it = set.begin(), end = set.end(); it != end; ++it) {
       *this << *it;
     }
     return *this;
@@ -100,8 +104,9 @@ class writer {
 
   template<typename K, typename V>
   writer &operator <<(const std::map<K, V> &map) {
-    scoped_object obj(*this);
-    for (typename std::map<K, V>::const_iterator it = map.begin(); it != map.end(); ++it) {
+    const scoped_object object(*this);
+    typedef typename std::map<K, V>::const_iterator const_iterator;
+    for (const_iterator it = map.begin(), end = map.end(); it != end; ++it) {
       *this << *it;
     }
     return *this;
@@ -113,17 +118,17 @@ class writer {
   class scoped_array {
    public:
     explicit scoped_array(writer &writer)
-      : _writer(writer) {
-      _writer.separator().put('[');
+        : _writer(writer) {
+      _writer.separator_and_clear().put('[');
     }
 
     scoped_array(writer &writer, const char *key)
-      : _writer(writer) {
-      (_writer.separator() << key).set_separator(false).put(':').put('[');
+        : _writer(writer) {
+      (_writer.separator_and_clear() << key).clear_separator().put(':').put('[');
     }
 
     virtual ~scoped_array() {
-      _writer.set_separator(false).put(']').set_separator(true);
+      _writer.put(']').set_separator();
     }
 
    private:
@@ -134,7 +139,7 @@ class writer {
    * \brief Write a JSON array to the underlying stream.
    */
   void add_array(const std::function<void(writer &)> &func) {
-    scoped_array obj(*this);
+    const scoped_array object(*this);
     func(*this);
   }
 
@@ -142,7 +147,7 @@ class writer {
    * \brief Write a keyed JSON array to the underlying stream.
    */
   void add_array(const char *key, const std::function<void(writer &)> &func) {
-    scoped_array arr(*this, key);
+    const scoped_array array(*this, key);
     func(*this);
   }
 
@@ -152,17 +157,17 @@ class writer {
   class scoped_object {
    public:
     explicit scoped_object(writer &writer)
-      : _writer(writer) {
-      _writer.separator().put('{');
+        : _writer(writer) {
+      _writer.separator_and_clear().put('{');
     }
 
     scoped_object(writer &writer, const char *key)
-      : _writer(writer) {
-      (_writer.separator() << key).set_separator(false).put(':').put('{');
+        : _writer(writer) {
+      (_writer.separator_and_clear() << key).clear_separator().put(':').put('{');
     }
 
     virtual ~scoped_object() {
-      _writer.set_separator(false).put('}').set_separator(true);
+      _writer.put('}').set_separator();
     }
 
    private:
@@ -173,7 +178,7 @@ class writer {
    * \brief Write a JSON object to the underlying stream.
    */
   void add_object(const std::function<void(writer &)> &func) {
-    scoped_object obj(*this);
+    const scoped_object object(*this);
     func(*this);
   }
 
@@ -181,26 +186,34 @@ class writer {
    * \brief Write a keyed JSON object to the underlying stream.
    */
   void add_object(const char *key, const std::function<void(writer &)> &func) {
-    scoped_object obj(*this, key);
+    const scoped_object object(*this, key);
     func(*this);
   }
 
  private:
-  /**
-   * \brief Conditionally write a separator to the underlying stream.
-   */
-  writer &separator() {
+  writer &separator_and_clear() {
     if (_separator_needed) {
       _stream << ',';
     }
-    return set_separator(false);
+    _separator_needed = false;
+    return *this;
   }
 
-  /**
-   * \brief Set the separator flag of this writer.
-   */
-  writer &set_separator(bool needed) {
-    _separator_needed = needed;
+  writer &separator_and_set() {
+    if (_separator_needed) {
+      _stream << ',';
+    }
+    _separator_needed = true;
+    return *this;
+  }
+
+  writer &clear_separator() {
+    _separator_needed = false;
+    return *this;
+  }
+
+  writer &set_separator() {
+    _separator_needed = true;
     return *this;
   }
 
@@ -218,7 +231,7 @@ class writer {
    */
   writer &write(const char *s, size_t n) {
     _stream.write(s, n);
-    return set_separator(false);
+    return *this;
   }
 
   /**
@@ -226,18 +239,18 @@ class writer {
    */
   writer &put(char c) {
     _stream.put(c);
-    return set_separator(false);
+    return *this;
   }
-
-  /**
-   * \brief Current separator flag.
-   */
-  bool _separator_needed;
 
   /**
    * \brief Underlying stream.
    */
   stream_type &_stream;
+
+  /**
+   * \brief Current separator flag.
+   */
+  bool _separator_needed;
 
   /**
    * \brief Scoped locale change.
