ecfeb19f5ac0b4359c0c6b822dcfd37cff9cfc96
Per Eckerdal <peck@spotify.com>
2015-06-04 13:23:00 +0200

    Fix key's copy constructors and assignment operators

--- test/test_key.cpp
+++ test/test_key.cpp
@@ -31,6 +31,18 @@ string to_string(const json::key &key) {
   return string(key.data, key.size);
 }
 
+key make_copied_key(const char *str) {
+  key a(str);
+  const key b(a);
+  return b;
+}
+
+key make_move_constructed_key(const char *str) {
+  key a(str);
+  const key b(std::move(a));
+  return b;
+}
+
 }  // anonymous namespace
 
 BOOST_AUTO_TEST_CASE(json_key_should_quote_raw_key) {
@@ -41,8 +53,38 @@ BOOST_AUTO_TEST_CASE(json_key_should_quote_string_key) {
   BOOST_CHECK_EQUAL(std::string("\"i_am_a_key\""), to_string(json::key("i_am_a_key")));
 }
 
+BOOST_AUTO_TEST_CASE(json_key_should_quote_raw_key_with_size) {
+  BOOST_CHECK_EQUAL("\"i_a\"", to_string(json::key("i_am_a_key", 3)));
+}
+
 BOOST_AUTO_TEST_CASE(json_key_should_escape_raw_key) {
   BOOST_CHECK_EQUAL("\"\\u0000\\\"\"", to_string(json::key(string("\0\"", 2))));
+}
+
+BOOST_AUTO_TEST_CASE(json_key_should_copy_correctly) {
+  const key k(make_copied_key("abcdef"));
+  BOOST_CHECK_EQUAL("\"abcdef\"", to_string(k));
+}
+
+BOOST_AUTO_TEST_CASE(json_key_should_move_construct_correctly) {
+  const key k(make_move_constructed_key("abcdef"));
+  BOOST_CHECK_EQUAL("\"abcdef\"", to_string(k));
+}
+
+BOOST_AUTO_TEST_CASE(json_key_should_assign_correctly) {
+  key a("abcdef");
+  const key b(a);
+  a = json::key("123456");
+
+  BOOST_CHECK_EQUAL("\"abcdef\"", to_string(b));
+}
+
+BOOST_AUTO_TEST_CASE(json_key_should_move_assign_correctly) {
+  key a("abcdef");
+  key b("123456");
+  a = std::move(b);
+
+  BOOST_CHECK_EQUAL("\"123456\"", to_string(a));
 }
 
 BOOST_AUTO_TEST_SUITE_END()  // json
