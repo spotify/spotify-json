0760217d7925c7930128b692a6b226eacf37be81
Johan Lindström <johanl@spotify.com>
2016-09-13 13:00:09 +0200

    Benchmark simple string encoding

--- benchmark/benchmark_string.cpp
+++ benchmark/benchmark_string.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Spotify AB
+ * Copyright (c) 2015-2016 Spotify AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -29,7 +29,8 @@ BOOST_AUTO_TEST_SUITE(json)
 BOOST_AUTO_TEST_SUITE(codec)
 
 std::string generate_simple_string(size_t size) {
-  std::string string("\"");
+  std::string string;
+  string.reserve(size);
   for (size_t i = 0; i < size; i++) {
     char c;
     switch (i % 3) {
@@ -39,16 +40,25 @@ std::string generate_simple_string(size_t size) {
     }
     string.append(&c, 1);
   }
-  string.append("\"");
   return string;
 }
 
+std::string generate_simple_json_string(size_t size) {
+  return "\"" + generate_simple_string(size) + "\"";
+}
+
+/*
+ * Decoding
+ */
+
 BOOST_AUTO_TEST_CASE(benchmark_json_codec_string_decode_simple_long_string) {
   const auto codec = default_codec<std::string>();
-  const auto json = generate_simple_string(10000);
+  const auto json = generate_simple_json_string(10000);
+  const auto json_begin = json.data();
+  const auto json_end = json.data() + json.size();
   JSON_BENCHMARK(1e5, [=]{
-    auto context = decoding_context(json.data(), json.data() + json.size());
-    codec.decode(context);
+    auto context = decoding_context(json_begin, json_end);
+    const auto decoded_string = codec.decode(context);
   });
 }
 
@@ -58,9 +68,35 @@ BOOST_AUTO_TEST_CASE(benchmark_json_codec_string_decode_simple_tiny_string) {
   const auto json_begin = json.data();
   const auto json_end = json.data() + json.size();
   JSON_BENCHMARK(1e5, [=]{
-    for (size_t i = 0; i < 10; i++) {
+    for (int i = 0; i < 100; i++) {
       auto context = decoding_context(json_begin, json_end);
-      codec.decode(context);
+      const auto decoded_string = codec.decode(context);
+    }
+  });
+}
+
+/*
+ * Encoding
+ */
+
+BOOST_AUTO_TEST_CASE(benchmark_json_codec_string_encode_simple_long_string) {
+  const auto codec = default_codec<std::string>();
+  const auto string = generate_simple_string(10000);
+  auto context = encoding_context(string.size() + 2);
+  JSON_BENCHMARK(1e5, [&]{
+    codec.encode(context, string);
+    context.clear();
+  });
+}
+
+BOOST_AUTO_TEST_CASE(benchmark_json_codec_string_encode_simple_tiny_string) {
+  const auto codec = default_codec<std::string>();
+  const auto string = std::string("spotify:track:05341EWu6uHUg2BojF3Cyw");
+  auto context = encoding_context(string.size() + 2);
+  JSON_BENCHMARK(1e5, [&]{
+    for (int i = 0; i < 100; i++) {
+      codec.encode(context, string);
+      context.clear();
     }
   });
 }
