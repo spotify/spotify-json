472314ce6affbcace9bbdc1781cf61412409e73f
Johan Lindström <johanl@spotify.com>
2019-10-27 23:20:53 +0100

    Move boolean_t implementation into .cpp file

--- include/spotify/json/codec/boolean.hpp
+++ include/spotify/json/codec/boolean.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 Spotify AB
+ * Copyright (c) 2015-2019 Spotify AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -16,16 +16,9 @@
 
 #pragma once
 
-#include <cstring>
-
 #include <spotify/json/decode_context.hpp>
 #include <spotify/json/default_codec.hpp>
-#include <spotify/json/detail/decode_helpers.hpp>
 #include <spotify/json/encode_context.hpp>
-
-#if _MSC_VER
-#pragma intrinsic (memcpy)
-#endif
 
 namespace spotify {
 namespace json {
@@ -35,21 +28,8 @@ class boolean_t final {
  public:
   using object_type = bool;
 
-  object_type decode(decode_context &context) const {
-    switch (detail::peek(context)) {
-      case 'f': detail::skip_false(context); return false;
-      case 't': detail::skip_true(context); return true;
-      default: detail::fail(context, "Unexpected input, expected boolean");
-    }
-  }
-
-  void encode(encode_context &context, const object_type value) const {
-    const auto needed = 5 - size_t(value);  // true: 4, false: 5
-    const auto buffer = context.reserve(needed);
-    memcpy(buffer, value ? "true" : "fals", 4);  // 4 byte writes optimize well on x86
-    buffer[needed - 1] = 'e'; // write the missing 'e' in 'false' (or overwrite it in 'true')
-    context.advance(needed);
-  }
+  object_type decode(decode_context &context) const;
+  void encode(encode_context &context, const object_type value) const;
 };
 
 inline boolean_t boolean() {
